26157914d6ae9c75feb804840c0840f68cf48fbd
jsjy96 <jsjy96@naver.com>
2021-12-31 15:08:33 +0900

    crop까지 성공

--- app/src/main/java/com/example/hwanghon/friend/MyProfileActivity.kt
+++ app/src/main/java/com/example/hwanghon/friend/MyProfileActivity.kt
@@ -1,6 +1,8 @@
 package com.example.hwanghon.friend
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -8,16 +10,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.example.hwanghon.R
 import com.example.hwanghon.board.BoardEditActivity
 import com.example.hwanghon.databinding.ActivityMyProfileBinding
 import com.example.hwanghon.utils.FBRef
+import com.github.drjacky.imagepicker.ImagePicker
 
 class MyProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMyProfileBinding
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            val uri = it.data?.data!!
+            // Use the uri to load the image
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -33,11 +44,27 @@ class MyProfileActivity : AppCompatActivity() {
         }
     }
 
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if(resultCode == RESULT_OK && requestCode == 100) {
+//            binding.profileimageArea.setImageURI(data?.data)
+//            binding.basicimageArea.visibility= View.INVISIBLE
+//        }
+//    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == RESULT_OK && requestCode == 100) {
-            binding.profileimageArea.setImageURI(data?.data)
+        if (resultCode == Activity.RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            val uri: Uri = data?.data!!
+
+            // Use Uri object instead of File to avoid storage permissions
+            binding.profileimageArea.setImageURI(uri)
             binding.basicimageArea.visibility= View.INVISIBLE
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(this, "사진 올리기를 실패하였습니다", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "사진 올리기를 취소하였습니다", Toast.LENGTH_LONG).show()
         }
     }
 
@@ -50,8 +77,18 @@ class MyProfileActivity : AppCompatActivity() {
         val alertDialog = mBuilder.show()
         alertDialog.findViewById<Button>(R.id.albumBtn)?.setOnClickListener {
 
-            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-            startActivityForResult(gallery, 100)
+
+
+            launcher.launch(
+                ImagePicker.with(this)
+                    .cropSquare()
+                    .galleryOnly()
+                    .maxResultSize(500, 500)
+                    .createIntent()
+            )
+
+//            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+//            startActivityForResult(gallery, 100)
 
             binding.profileimageArea.visibility= View.VISIBLE
 
@@ -65,5 +102,6 @@ class MyProfileActivity : AppCompatActivity() {
             alertDialog.dismiss()
         }
     }
+
 
 }
